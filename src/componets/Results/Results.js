a5927da32aea1bab45092d9291bcf32ee4ce3e42
stsp93 <stspsp@gmail.com>
2023-03-06 22:26:20 +0200

    added loading message while fetching results

--- src/componets/Results/Results.js
+++ src/componets/Results/Results.js
@@ -10,12 +10,18 @@ export default function Category() {
     const { category } = useParams();
     const [searchParams] = useSearchParams();
     const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     // On category show
     useEffect(() => {
         (async () => {
+            // Start fetch
+            setLoading(true);
             const categoryResults = await getCategoryResults(category || '');
             setResults(categoryResults);
+
+            // End fetch
+            setLoading(false);
         })()
     }, [category]);
 
@@ -25,26 +31,39 @@ export default function Category() {
         const query = searchParams.get('q')
         if (query) {
             (async function () {
+                // Start fetch
+                setLoading(true);
                 const res = await searchItems({ q: query });
                 setResults(res);
+
+                // End fetch
+                setLoading(false);
             })()
         }
-    }, [searchParams])
+    }, [searchParams]);
+
+    // Title with Results count
+    function showTitle(results, loading) {
+        if (loading) {
+            return <h1>Loading...</h1>
+        }
+        if (results.length) {
+            return <h2 className="title main-title">
+                {results.length} Result{results.length === 1 ? '' : 's'} {searchParams.get('q') && `for '${searchParams.get('q')}'`}
+            </h2>
+        }
+        // No Results
+        return <>
+            <h2 className="title main-title">No Results {searchParams.get('q') && `for '${searchParams.get('q')}'`}</h2>
+            <Carousel />
+        </>
+    }
 
     return (
         <>
-            {results.length ?
-                <h2 className="title main-title">
-                    {results.length} Result{results.length === 1 ? '': 's'} {searchParams.get('q') && `for '${searchParams.get('q')}'`}
-                </h2>
-                : <>
-                    <h2 className="title main-title">No Results {searchParams.get('q') && `for '${searchParams.get('q')}'`}</h2>
-                    <Carousel />
-                </>}
-
+            {showTitle(results, loading)}
             <ul className="offers-list">
                 {results.map(res => <OfferCard key={res._id} {...res} />)}
-
             </ul>
 
             {/* <!-- check if 0 offers  --> */}
