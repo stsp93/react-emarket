eacbf8e3a78a4dd49b3bb567cf5f5b5c080a000e
stsp93 <stspsp@gmail.com>
2023-03-14 23:53:12 +0200

    added header comments and refactor

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -14,21 +14,26 @@ export default function Header() {
     const navigate = useNavigate();
     const showLoading = useLoading();
 
-    // Active nav links styling
-    const [activeStatus, setActive] = useState(false);
-
-    function handleNavButton() {
-        setActive(state => !state);
-    }
-
+    //Setting active link
     const activeStyle = ({ isActive }) => isActive
-        ? { color: 'var(--zomp)' }
-        : undefined;
+    ? { color: 'var(--zomp)' }
+    : undefined;
 
-    const liClassName = (activeStatus) => {
-        // return `nav__li ${activeStatus ? 'nav__active' : ''}`
-        return `nav__li ${activeStatus ? 'nav__active' : ''}`
+
+    // Mobile view logic
+
+    const [visible, setVisible] = useState(false);
+
+    // Show/Hide Nav Button (mobile view only)
+    function handleNavButton() {
+        setVisible(state => !state);
     }
+
+    //Show/Hide links className (mobile view only)
+    const liClassName = (visible) => {
+        return `nav__li ${visible ? 'nav__visible' : ''}`
+    }
+
 
     // Search logic
     const [query, setQuery] = useState('');
@@ -69,19 +74,19 @@ export default function Header() {
                     <button onClick={handleNavButton} className="nav__button"><i className="fa-solid fa-bars"></i></button>
                     <ul>
                 <em className='nav__link'>{auth && auth.email}</em>
-                        <li className={liClassName(activeStatus)}><NavLink className='nav__link' style={activeStyle} to="/"><i className="fa-solid fa-list-ul"></i>Categories</NavLink></li>
+                        <li className={liClassName(visible)}><NavLink className='nav__link' style={activeStyle} to="/"><i className="fa-solid fa-list-ul"></i>Categories</NavLink></li>
                         {auth ?
                             <>
-                                {/* <!-- USER --> */}
-                                <li className={liClassName(activeStatus)}><NavLink className='nav__link' style={activeStyle} to="/user/profile"><i className="fa-solid fa-user"></i>Profile</NavLink></li>
-                                <li className={liClassName(activeStatus)}><button className='nav__link' onClick={onLogout}><i className="fa-solid fa-right-from-bracket"></i>Logout</button></li>
+                                {/* <!-- USER LINKS --> */}
+                                <li className={liClassName(visible)}><NavLink className='nav__link' style={activeStyle} to="/user/profile"><i className="fa-solid fa-user"></i>Profile</NavLink></li>
+                                <li className={liClassName(visible)}><button className='nav__link' onClick={onLogout}><i className="fa-solid fa-right-from-bracket"></i>Logout</button></li>
                             </>
                             : <>
-                                {/* <!-- GUEST --> */}
-                                <li className={liClassName(activeStatus)}>
+                                {/* <!-- GUEST LINKS --> */}
+                                <li className={liClassName(visible)}>
                                     <button className='nav__link' to="#" onClick={updateModal.bind(this, 'Login')}><i className="fa-solid fa-user-check"></i>Login</button>
                                 </li>
-                                <li className={liClassName(activeStatus)}>
+                                <li className={liClassName(visible)}>
                                     <button className='nav__link' to="#" onClick={updateModal.bind(this, 'Register')} ><i className="fa-solid fa-file-signature"></i>Register</button>
                                 </li>
                             </>
