765604df5fa0e1467ad67ac9df606bb4f2009f00
stsp93 <stspsp@gmail.com>
2023-03-14 22:53:00 +0200

    added useLoading hook

---
+++ src/hooks/useLoading.js
@@ -0,0 +1,20 @@
+// export default function showLoading(updateModal, closeModal) {
+//                 // Show Loading and remove it after 2s
+
+// }
+import { useContext } from 'react';
+import { ModalContext } from '../context/ModalContext';
+
+
+
+export function useLoading() {
+    const { updateModal, closeModal } = useContext(ModalContext)
+
+    return () => {
+        updateModal('Loading');
+
+        setTimeout(() => {
+            closeModal()
+        }, 2000)
+    }
+}
--- src/utils/showLoading.js
+++
@@ -1,8 +0,0 @@
-export default function showLoading(updateModal, closeModal) {
-                // Show Loading and remove it after 2s
-                updateModal('Loading');
-
-                setTimeout(() => {
-                    closeModal()
-                }, 2000)
-}
