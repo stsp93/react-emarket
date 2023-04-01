dc569c32b3bd628053f13775d22626395271478a
stsp93 <stspsp@gmail.com>
2023-04-01 11:30:06 +0300

    added results sorting

--- src/componets/Results/Results.js
+++ src/componets/Results/Results.js
@@ -1,9 +1,12 @@
+import './Results.css'
+
 import { useParams, useSearchParams } from 'react-router-dom';
 import { getCategoryResults, searchItems } from '../../services/api/data';
 import { useState, useEffect } from 'react';
 
 import Carousel from '../common/Carousel/Carousel';
 import OfferList from '../common/OfferList/OfferList';
+import Loading from './../Modal/Loading/Loading';
 
 
 export default function Results() {
@@ -19,7 +22,7 @@ export default function Results() {
             setLoading(true);
             const categoryResults = await getCategoryResults(category || '');
             setResults(categoryResults);
-
+            console.log(results);
             // End fetch
             setLoading(false);
         })()
@@ -34,7 +37,7 @@ export default function Results() {
             (async function () {
                 // Start fetch
                 setLoading(true);
-                const res = await searchItems({ q: query, cat:category });
+                const res = await searchItems({ q: query, cat: category });
                 setResults(res);
 
                 // End fetch
@@ -43,15 +46,37 @@ export default function Results() {
         }
     }, [searchParams]);
 
+    // on Sort
+
+    const sortingMap = {
+        'newest': () => setResults(res => [...res.sort((a, b) => new Date(b.createdOn) - new Date(a.createdOn))]),
+        'oldest': () => setResults(res => [...res.sort((a, b) => new Date(a.createdOn) - new Date(b.createdOn))]),
+        'cheap': () => setResults(res => [...res.sort((a, b) => a.price - b.price)]),
+        'expensive': () => setResults(res => [...res.sort((a, b) => b.price - a.price)]),
+    }
+
+    function sortBy(e) {
+        sortingMap[e.target.value]()
+    }
+
     // Title with Results count
     function showTitle(results, loading) {
         if (loading) {
-            return <h1>Loading...</h1>
+            return <Loading />
         }
         if (results.length) {
-            return <h2 className="title main-title">
-                {results.length} Result{results.length === 1 ? '' : 's'} {searchParams.get('q') && `for '${searchParams.get('q')}'`}
-            </h2>
+            return (
+                <div className='title__wrapper'>
+                    <h2 className="title main-title">
+                        {results.length} Result{results.length === 1 ? '' : 's'} {searchParams.get('q') && `for '${searchParams.get('q')}'`}
+                    </h2>
+                    <select onChange={sortBy} className='results__sort' name="sort-by" id="sort-by">
+                        <option value="newest">Show newest</option>
+                        <option value="oldest">Show oldest</option>
+                        <option value="cheap">Show cheapest</option>
+                        <option value="expensive">Show most expensive</option>
+                    </select>
+                </div>)
         }
         // No Results
         return <>
